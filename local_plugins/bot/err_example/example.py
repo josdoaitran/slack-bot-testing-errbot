d57823e56259d79095e03f3e0a6c9def5f1f1eab
josdoaitran <josdoaitran@gmail.com>
2020-10-18 10:59:03 +0700

    example return thread

--- local_plugins/bot/err_example/example.py
+++ local_plugins/bot/err_example/example.py
@@ -9,7 +9,7 @@ class Example(BotPlugin):
     @botcmd
     def example_direct_return_name(self, msg, data):
         """
-        Usage:
+        Usage: !example direct return name <firstName> <lastName>
         """
         if not data:
             return 'Usage: !example direct return <firstName> <lastName>'
@@ -23,3 +23,22 @@ class Example(BotPlugin):
 
         return ':get_well_soon: Your FulName is `' + lastName + '` ' + '`' + firstName + '`'
 
+    @botcmd()
+    def example_thread_return_name(self, msg, data):
+        """
+               Usage: !example thread return name <firstName> <lastName>
+               """
+        if not data:
+            return 'Usage: !example thread return name <firstName> <lastName>'
+        try:
+            all_data = str(data)
+            cmd_split = all_data.split(" ")
+            firstName = cmd_split[0].strip()
+            lastName = cmd_split[1].strip()
+        except:
+            return 'Usage: !example thread return name <firstName> <lastName>'
+
+        self.send(msg.to, ':hammer_and_wrench: Your FulName is `' + lastName + '` ' + '`' + firstName + '`', in_reply_to=msg)
+
+    # @botcmd()
+    # def example_return_card(self, msg, data):
